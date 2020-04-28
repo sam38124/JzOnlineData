4847bc00939d27fc66aafd612bdeaa98f37ef2f5
sam38124 <sam38124@gmail.com>
2020-04-28 06:25:40 -0700

    0625

--- app/src/main/java/com/example/jzonlinedatasample/MainActivity.kt
+++ app/src/main/java/com/example/jzonlinedatasample/MainActivity.kt
@@ -3,15 +3,16 @@ package com.example.jzonlinedatasample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.example.jzfixlanguage.OnlineData
-import com.example.jzfixlanguage.callback
-import com.example.jzfixlanguage.getOnlineData
+import com.example.jzonlinedata.OnlineData
+import com.example.jzonlinedata.callback
+import com.example.jzonlinedata.getOnlineData
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         OnlineData.newInstance.setUP(this,"https://bento2.orange-electronic.com/Orange%20Cloud/OnlineData/Oglite.txt",object :
             callback {
             override fun result(a: Boolean) {
--- jzonlinedata/src/main/java/com/example/jzonlinedata/OnlineData.kt
+++ jzonlinedata/src/main/java/com/example/jzonlinedata/OnlineData.kt
@@ -1,14 +1,13 @@
-package com.example.jzfixlanguage
+package com.example.jzonlinedata
 
 import android.content.Context
 import android.util.Log
+import com.example.jzfixlanguage.converData
 import com.example.jztaskhandler.TaskHandler
 import com.example.jztaskhandler.runner
 import com.jzsql.lib.mmySql.JzSqlHelper
 import com.jzsql.lib.mmySql.Sql_Result
-import java.io.BufferedInputStream
 import java.io.BufferedReader
-import java.io.DataInputStream
 import java.io.InputStreamReader
 import java.lang.Exception
 import java.net.HttpURLConnection
@@ -16,29 +15,29 @@ import java.net.URL
 
 class OnlineData {
     companion object{
-        private var instance:OnlineData? = null
-        val newInstance:OnlineData
+        private var instance: OnlineData? = null
+        val newInstance: OnlineData
         get() {
-            if(instance==null){
-                instance=OnlineData()
+            if(instance ==null){
+                instance = OnlineData()
             }
             return instance!!
         }
 
     }
-    fun  setUP(context:Context,url:String,result:callback){
-        sqlHelper=JzSqlHelper(context,"language").create()
-        sqlHelper.exsql("CREATE TABLE if not exists fixlan (\n" +
+    fun  setUP(context:Context,url:String,result: callback){
+        sqlHelper=JzSqlHelper(context,"onlinedata").create()
+        sqlHelper.exsql("CREATE TABLE if not exists onlinedata (\n" +
                 "    repl   VARCHAR PRIMARY KEY,\n" +
                 "    replto VARCHAR\n" +
                 ");\n")
 
         val profilePreferences =context.getSharedPreferences("Setting", Context.MODE_PRIVATE)
-        val versionNow=profilePreferences.getString("fixLanVersion","nodata")
+        val versionNow=profilePreferences.getString("onlinedata","nodata")
         Log.e("versionNow",versionNow)
         Thread{
 
-            TaskHandler.newInstance.runTaskOnce("getFix", runner {
+            TaskHandler.newInstance.runTaskOnce("onlinedata", runner {
                 try{
                     val url: HttpURLConnection =(URL(url).openConnection()) as HttpURLConnection
                     url.connectTimeout=10*1000
@@ -50,26 +49,26 @@ class OnlineData {
                     while (a != null){
                         if(first){
                             first=false
-                            if(versionNow==converData.Data(a)[0].value){
+                            if(versionNow== converData.Data(a)[0].value){
                                 break
                             }else{
-                                sqlHelper.dropTb("fixlan")
-                                sqlHelper.exsql("CREATE TABLE if not exists fixlan (\n" +
+                                sqlHelper.dropTb("onlinedata")
+                                sqlHelper.exsql("CREATE TABLE if not exists onlinedata (\n" +
                                         "    repl   VARCHAR PRIMARY KEY,\n" +
                                         "    replto VARCHAR\n" +
                                         ");\n")
-                                version=converData.Data(a)[0].value
-                                sqlHelper.exsql("insert or replace into fixlan(repl,replto) values ('version','${version}')")
+                                version= converData.Data(a)[0].value
+                                sqlHelper.exsql("insert or replace into onlinedata(repl,replto) values ('version','${version}')")
                             }
                         }else{
-                            val s=converData.Data(a)
+                            val s= converData.Data(a)
                             for(data in s){
-                                sqlHelper.exsql("insert or replace into fixlan(repl,replto) values ('${data.name.replace("'","''")}','${data.value.replace("'","''")}')")
+                                sqlHelper.exsql("insert or replace into onlinedata(repl,replto) values ('${data.name.replace("'","''")}','${data.value.replace("'","''")}')")
                             }
                         }
                         a=reader.readLine()
                     }
-                    profilePreferences.edit().putString("fixLanVersion",version).commit()
+                    profilePreferences.edit().putString("onlinedata",version).commit()
                     result.result(true)
                 }catch (e:Exception){e.printStackTrace()
                     result.result(false)
